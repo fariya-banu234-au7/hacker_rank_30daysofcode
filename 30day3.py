0fc57844f218ef3f7ee1d1eec69605da04e9bcad
Fariya Banu <fariyafortunate23@gmail.com>
2020-02-16 18:09:07 +0530

    day3

---
+++ 30day3.py
@@ -0,0 +1,11 @@
+N = int(input())
+if (N % 2 != 0):
+    print("Weird")
+
+elif (N % 2 == 0):
+    if N in range(2,5):
+        print("Not Weird")
+    elif N in range(6,20):
+        print("Weird")
+    elif N > 20:
+        print("Not weird")
